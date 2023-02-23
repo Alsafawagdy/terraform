bd544819c8ddbf590a5b8f511af6aaecb3e8523c
Alsafawagdy <71265897+Alsafawagdy@users.noreply.github.com>
2023-02-23 15:05:19 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,4 @@
 provider "aws" {
   # region = var.aws_region
   # shared_credentials_files = "~/.aws/credentials"
-  access_key = "AKIAWERX6SIVZSPA25E3"
-  secret_key = "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"
 }
